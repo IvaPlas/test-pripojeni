996f5f180c2791d9013763e9cae2de06672de5f9
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-09-29 21:12:15 +0200

    cleanup function set up

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,15 +3,32 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  if (isOnline) {
-    return (
-      <p>Všechno v pořádku</p>
-    )
+  const handleOffline = () => {
+    console.log('You are offline.');
+    setIsOnline(false);
   };
 
-  return (
-    <p>Jste offline!</p>
-  );
+  const handleOnline = () => {
+    console.log('You are online.');
+    setIsOnline(true);
+  };
+
+  useEffect(() => {
+    console.log('mounted');
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+    return () => {
+      console.log('umounted');
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return <p>Všechno v pořádku</p>;
+  }
+
+  return <p>Jste offline!</p>;
 };
 
 export default Offline;
